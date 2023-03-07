a83b35a278f902c9d7ab0fb655c36438bff0ba8a
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-07 17:11:21 +0300

    Add missionsdetails

--- src/routes/mission.js
+++ src/routes/mission.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import MissionList from 'components/missionList';
+import styles from 'styles/Mission.module.css';
 
 function Missions() {
-  <h1>This are the missions</h1>;
+  return (
+    <div className={styles.missionsContainer}>
+      <div className={styles.missionsHeader}>
+        <div>
+          <p>Mission</p>
+        </div>
+        <div>
+          <p>Description</p>
+        </div>
+        <div>
+          <p>Status</p>
+        </div>
+        <div />
+      </div>
+      <MissionList />
+    </div>
+  );
 }
 
 export default Missions;
