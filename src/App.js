9d1ab06b70f0b3229b4eb0708fe523e5335f0ccd
mrprotocoll <lekanvgbg@gmail.com>
2023-03-07 16:28:23 +0100

    Created rockets route display

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import BooksList from 'routes/mission';
+import Mission from 'routes/mission';
 import Navbar from './components/Navbar';
 import Profile from './routes/profile';
-import Rocket from './routes/rocket';
+import Rockets from './routes/rockets';
 
 const App = () => (
   <div className="app">
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<BooksList />} />
-        <Route path="/categories" element={<Rocket />} />
-        <Route path="/categories" element={<Profile />} />
+        <Route path="/" element={<Rockets />} />
+        <Route path="/mission" element={<Mission />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   </div>
---
+++ src/routes/rockets.js
@@ -0,0 +1,9 @@
+import Rocket from 'components/Rocket';
+
+const Rockets = () => (
+  <ul>
+    <Rocket id="1" name="Rocket 1" descripton="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo voluptas non totam est beatae doloremque nemo, delectus nobis sapiente voluptate atque accusantium, at quis error dolore asperiores possimus ratione libero." image="https://media.istockphoto.com/id/1394951609/photo/space-shuttle-and-launch-rockets-leave-the-earths-atmosphere.jpg?s=612x612&w=0&k=20&c=KjH6bMGbIdeX_wZ5UI9dATzq0UsUxjM90MlJC-XPmnk=" />
+  </ul>
+);
+
+export default Rockets;
