bbbc20e65ec5a3f1f745e88dc9e009cd1861022a
mrprotocoll <lekanvgbg@gmail.com>
2023-03-08 11:03:49 +0100

    Add reducer to get rockets

--- src/features/Rockets/Rockets.js
+++ src/features/Rockets/Rockets.js
@@ -1,3 +1,5 @@
+import getRockets from './RocketsThunk';
+
 const { createSlice } = require('@reduxjs/toolkit');
 
 const initialState = {
@@ -7,6 +9,11 @@ const RocketSlice = createSlice({
   name: 'rocket',
   initialState,
   reducers: {},
+  extraReducers: {
+    [getRockets.fulfilled]: (state, { payload }) => {
+      state.rockets = payload;
+    },
+  },
 });
 
 export default RocketSlice.reducer;
