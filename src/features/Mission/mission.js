4877667cf671069b023f6bbeb97ecf4119f73ba1
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-08 13:58:46 +0300

    Update Actions

--- src/features/Mission/mission.js
+++ src/features/Mission/mission.js
@@ -1,13 +1,58 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const initialState = {
-  mission: [],
-};
+// Actions
+const JoinMission = 'spaceTravelers/mission/JoinMission';
+const LeaveMission = 'spaceTravelers/mission/Remove';
+const Fetchmission = 'spaceTravelers/mission/Fetchmission';
 
-const missionSlice = createSlice({
-  name: 'mission',
-  initialState,
-  reducers: {},
+// API
+const URL = 'https://api.spacexdata.com/v3/missions';
+
+const initialState = [];
+
+// Reducer
+export default function missionReducer(state = initialState, action) {
+  switch (action.type) {
+    case JoinMission:
+      return state.map((mission) => {
+        if (mission.mission_id !== action.payload) return mission;
+        return { ...mission, joined: true };
+      });
+
+    case LeaveMission:
+      return state.map((mission) => {
+        if (mission.mission_id !== action.payload) return mission;
+        return { ...mission, joined: false };
+      });
+
+    case Fetchmission:
+      return action.payload;
+    default:
+      return [...state];
+  }
+}
+
+// Actions creators
+export const joinMission = (payload) => ({
+  type: JoinMission,
+  payload,
 });
 
-export default missionSlice.reducer;
+export const leaveMission = (payload) => ({
+  type: LeaveMission,
+  payload,
+});
+
+// Fetch Missions
+export const getMissions = createAsyncThunk(
+  Fetchmission,
+  async (_, { dispatch }) => {
+    const response = await fetch(URL);
+    const objectData = await response.json();
+    const newObj = objectData.map((mission) => ({ ...mission, joined: false }));
+    dispatch({
+      type: Fetchmission,
+      payload: newObj,
+    });
+  },
+);
--- src/routes/mission.js
+++ src/routes/mission.js
@@ -1,25 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MissionList from 'components/missionList';
 import styles from 'styles/MissionList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMissions } from 'features/Mission/mission';
 
-function Missions() {
+let render = true;
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missionReducer);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!render) {
+      return;
+    }
+    render = false;
+    dispatch(getMissions());
+  }, [dispatch]);
+
   return (
-    <div className={styles.missionsContainer}>
-      <div className={styles.missionsHeader}>
-        <div>
-          <p>Missions</p>
-        </div>
-        <div>
-          <p>Descriptions</p>
-        </div>
-        <div>
-          <p>Status</p>
-        </div>
-        <div />
-      </div>
-      <MissionList />
+    <div className="container">
+      <table className={styles.table}>
+        <tbody>
+          <tr>
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>.</th>
+            <td />
+          </tr>
+          {missions.map((mission) => (
+            <MissionList
+              key={mission.mission_id}
+              id={mission.mission_id}
+              name={mission.mission_name}
+              description={mission.description}
+              joined={mission.joined}
+            />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-}
-
+};
 export default Missions;
