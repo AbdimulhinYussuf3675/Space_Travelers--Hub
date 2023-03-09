052ef329a1c80d8c74ce49ace94092307318a16d
mrprotocoll <lekanvgbg@gmail.com>
2023-03-09 15:33:37 +0100

    refactored code

--- src/features/Rockets/Rockets.js
+++ src/features/Rockets/Rockets.js
@@ -2,27 +2,23 @@ import getRockets from './RocketsThunk';
 
 const { createSlice } = require('@reduxjs/toolkit');
 
-const initialState = {
-  rockets: [],
-};
 const RocketSlice = createSlice({
   name: 'rocket',
-  initialState,
+  initialState: [],
   reducers: {
-    reserveRocket: (state, { payload }) => {
-      state.rockets = state.rockets.map((rocket) => ((rocket.id === payload)
-        ? { ...rocket, reserved: !rocket.reserved } : rocket));
-    },
+    reserveRocket: (state, { payload }) => state.map((rocket) => ((rocket.id === payload)
+      ? { ...rocket, reserved: !rocket.reserved } : rocket)),
   },
   extraReducers: {
     [getRockets.fulfilled]: (state, { payload }) => {
-      state.rockets = payload.map((item) => ({
+      state = payload.map((item) => ({
         id: item.id,
         name: item.rocket_name,
         description: item.description,
         image: item.flickr_images[0],
         reserved: false,
       }));
+      return state;
     },
   },
 });
