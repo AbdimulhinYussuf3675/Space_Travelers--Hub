876e172da17657438806c60c4a86b903f1198a9d
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-03-09 14:15:00 +0300

    Add mission in profiles

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import { getMissions } from 'features/Mission/mission';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import styles from '../styles/Profilemiss.module.css';
 
-function Profile() {
-  <h1>This are the Profile page</h1>;
-}
+let render = true;
+const Profile = () => {
+  const missions = useSelector((state) => state.missionReducer);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (!render) {
+      return;
+    }
+    render = false;
+    dispatch(getMissions());
+  }, [dispatch]);
+
+  const newProf = missions.filter((mission) => mission.joined === true);
+
+  const profile = (prof) => {
+    let output = '';
+    if (prof.length === 0) {
+      output = <div className={styles.noMission}>No Mission Available</div>;
+    } else {
+      output = prof.map((mission) => (
+        <li key={mission.mission_id} className={styles.missionItems}>
+          {mission.mission_name}
+        </li>
+      ));
+    }
+    return output;
+  };
+
+  return (
+    <div className={styles.myProfile}>
+      <div className={styles.missionContainer}>
+        <h2>My Missions</h2>
+        <ul>{profile(newProf)}</ul>
+      </div>
+    </div>
+  );
+};
 
 export default Profile;
